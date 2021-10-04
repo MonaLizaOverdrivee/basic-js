19c673cac63822e714ad95113690a592df42efc1
MonaLizaOverdrivee <santospele@mail.ru>
2021-10-04 17:00:26 +0300

    count-cats, dream, team

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ import { NotImplementedError } from '../extensions/index.js';
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,7 +14,10 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+export default function countCats(matrix) {
+  return matrix.reduce((acc, itm) => {
+    acc += itm.filter(sub => sub === '^^').length
+    return acc
+  }, 0)
   // remove line with error and write your code here
 }
--- src/dream-team.js
+++ src/dream-team.js
@@ -2,18 +2,19 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  return members instanceof Array ? members.map(itm => {
+    if(typeof itm === 'string') return itm.trim()[0].toUpperCase()
+  }).sort().join('') : false
 }
